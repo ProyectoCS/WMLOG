fce919800367c3d4427a0ad2e8430f05cd119e83
Jose Luis Alonso Contreras <joseluis@macbook-pro.gateway.2wire.net>
2014-04-22 16:03:01 -0500

    Creacion del proyecto WMLOG

---
+++ WMLOG/WMLOG/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WMLOG
+//
+//  Created by Jose Luis Alonso Contreras on 22/04/14.
+//  Copyright (c) 2014 CLICKSOFT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
